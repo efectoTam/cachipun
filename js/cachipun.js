1e418c00b67dd4bcfa146cb0206deaf6d2e3f5d0
Tamara Luque <tam.luque@gmail.com>
2016-10-04 13:28:04 -0300

    Agregué comandos para que el usuario ingrese solo piedra, papel o tijera, para que no importe si hay problemas de mayúscula y minúscula, y para que el programa pregunte si quieres seguir jugando o no

--- js/cachipun.js
+++ js/cachipun.js
@@ -2,51 +2,65 @@
 
 var jugadaUser;
 var jugadaCompu;
+var numRandom;
+var numRedondo;
 
-var jugadaUser = prompt("¿piedra, papel o tijera?");
-console.log(jugadaUser);
+//la condición para detenerse es que el input sea piedra, papel o tijera
 
-/*Busco un número random para la selección de la opción de la computadora*/
-var numRandom=Math.random()*2;
-var numRedondo=Math.round(numRandom);
-console.log(numRedondo);
+do{
+	while(!(jugadaUser==="piedra" || jugadaUser==="papel" || jugadaUser==="tijera")){
+		//acá pregunto al usuario
+		var jugadaUser = prompt("¿piedra, papel o tijera?").toLowerCase();
+		console.log(jugadaUser);
+	}
 
-/*0: piedra / 1:tijera / 2:papel*/
+	/*Busco un número random para la selección de la opción de la computadora*/
+	numRandom=Math.random()*2;
+	numRedondo=Math.round(numRandom);
+	console.log(numRedondo);
 
-if (numRedondo==0){
-	jugadaCompu="piedra";
-} else if (numRedondo==1){
-	jugadaCompu="tijera";
-} else {
-	jugadaCompu="papel";
+	/*0: piedra / 1:tijera / 2:papel*/
+
+	if (numRedondo==0){
+		jugadaCompu="piedra";
+	} else if (numRedondo==1){
+		jugadaCompu="tijera";
+	} else {
+		jugadaCompu="papel";
+	}
+
+	alert("Ahora le toca al computador elegir");
+	console.log(jugadaCompu);
+	alert("El computador eligió: " + jugadaCompu);
+
+	if (jugadaUser=="piedra"){
+		if (jugadaCompu=="piedra"){
+			alert("¡Empate!");
+		} else if(jugadaCompu=="papel"){
+			alert("¡La computadora gana!");
+		} else {
+			alert("¡Ganaste!");
+		}
+	} else if (jugadaUser=="papel"){
+		if (jugadaCompu=="piedra"){
+			alert("¡Ganaste!");
+		} else if (jugadaCompu=="papel"){
+			alert("¡Empate!");
+		} else {
+			alert("¡La computadora gana!");
+		}
+	} else if(jugadaUser=="tijera"){
+		if (jugadaCompu=="piedra"){
+			alert("¡La computadora gana!");
+		} else if (jugadaCompu=="papel"){
+			alert("¡Ganaste!");
+		} else {
+			alert("¡Empate!");
+		}
+	} else {
+		alert("La palabra no corresponde");
+	}
+	jugadaUser ="Cualquier cosa";
+	var confirmacion= window.confirm("¿Desea jugar de nuevo?");
 }
-
-alert("Ahora le toca al computador elegir");
-console.log(jugadaCompu);
-alert("El computador eligió: " + jugadaCompu);
-
-if (jugadaUser=="piedra"){
-	if (jugadaCompu=="piedra"){
-		alert("¡Empate!");
-	} else if(jugadaCompu=="papel"){
-		alert("¡Computador gana!");
-	} else {
-		alert("¡Usuario gana!");
-	}
-} else if (jugadaUser=="papel"){
-	if (jugadaCompu=="piedra"){
-		alert("¡Usuario gana!");
-	} else if (jugadaCompu=="papel"){
-		alert("¡Empate!");
-	} else {
-		alert("¡Computador gana!");
-	}
-} else {
-	if (jugadaCompu=="piedra"){
-		alert("¡Computador gana!");
-	} else if (jugadaCompu=="papel"){
-		alert("¡Usuario gana!");
-	} else {
-		alert("¡Empate!");
-	}
-}
+while(confirmacion==true);
